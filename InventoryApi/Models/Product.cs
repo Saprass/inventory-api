42cdd4b4335c2e31b5e9d66971f37e5eccf63ef5
Saprass <cruz_y_raya_2@hotmail.com>
2026-02-05 12:48:33 +0100

    Add Product model

---
+++ InventoryApi/Models/Product.cs
@@ -0,0 +1,13 @@
+
+namespace InventoryApi.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+    public string? Description { get; set; }
+    public int Stock { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
