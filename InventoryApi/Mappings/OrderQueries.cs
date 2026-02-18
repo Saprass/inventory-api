35790d993e8f625b5991c2d6b4672d3b830fd036
Saprass <cruz_y_raya_2@hotmail.com>
2026-02-18 13:58:36 +0100

    Add OrderQueries for Order projection mapping

---
+++ InventoryApi/Mappings/OrderQueries.cs
@@ -0,0 +1,37 @@
+﻿using InventoryApi.DTOs.Common;
+using InventoryApi.DTOs.Orders;
+using InventoryApi.Models;
+
+namespace InventoryApi.Mappings;
+
+public static class OrderQueries
+{
+    public static IQueryable<OrderSummaryDTO> SelectOrderSummary(this IQueryable<Order> q) =>
+       q.Select(o => new OrderSummaryDTO(
+           o.Id,
+        new CustomerInfoDTO(
+            o.Customer.Id,
+            o.Customer.Name
+        ),
+        o.OrderDate,
+        o.OrderStatus.ToString(),
+        o.OrderItems.Count,
+        o.TotalAmount
+       ));
+
+    public static IQueryable<OrderDetailDTO> SelectOrderDetail(this IQueryable<Order> q, int id) =>
+    q.Where(o => o.Id == id)
+        .Select(o => new OrderDetailDTO(
+        o.Id,
+        new CustomerInfoDTO(o.Customer.Id, o.Customer.Name),
+        o.OrderDate,
+        o.OrderStatus.ToString(),
+        o.TotalAmount,
+        o.OrderItems.Select(oi => new OrderItemDTO(
+            oi.Id,
+            new ProductInfoDTO(oi.Product.Id, oi.Product.Name),
+            oi.Quantity,
+            oi.UnitPrice
+        )).ToList()
+    ));
+}
